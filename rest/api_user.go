f9b006bdacd254cf39482ff99637e4e7e511f6c2
mendelgel <37910245+Menahem-Mendel@users.noreply.github.com>
2020-05-21 02:26:45 +0300

    model clear

--- rest/api_user.go
+++ rest/api_user.go
@@ -6,10 +6,9 @@ package rest
 
 // func (c Client) DeleteUserWithdrawal(ctx context.Context, token string) (models.Transaction, error) {}
 
-// UserCheckReferralCodeConf
-type UserCheckReferralCodeConf struct {
-	ReferralCode string
-}
+// type UserCheckReferralCodeConf struct {
+// 	ReferralCode string
+// }
 
 // func (c Client) UserCheckReferralCode(ctx context.Context, localVarOptionals *UserCheckReferralCodeOpts) (float64, *http.Response, error) {}
 
@@ -21,64 +20,56 @@ type UserCheckReferralCodeConf struct {
 
 // func (c Client) UserGetCommission(ctx context.Context) (UserCommissionsBySymbol, *http.Response, error) {}
 
-// UserGetDepositAddressConf
-type UserGetDepositAddressConf struct {
-	Currency string
-}
+// type UserGetDepositAddressConf struct {
+// 	Currency string
+// }
 
 // func (c Client) UserGetDepositAddress(ctx context.Context, localVarOptionals *UserGetDepositAddressOpts) (string, *http.Response, error) {}
 
 // func (c Client) UserGetExecutionHistory(ctx context.Context, symbol string, timestamp time.Time) (interface{}, *http.Response, error) {}
 
-// UserGetMarginConf
-type UserGetMarginConf struct {
-	Currency string
-}
+// type UserGetMarginConf struct {
+// 	Currency string
+// }
 
 // func (c Client) UserGetMargin(ctx context.Context, localVarOptionals *UserGetMarginOpts) (Margin, *http.Response, error) {}
 
 // func (c Client) UserGetQuoteFillRatio(ctx context.Context) (QuoteFillRatio, *http.Response, error) {}
 
-// UserGetWalletConf
-type UserGetWalletConf struct {
-	Currency string
-}
+// type UserGetWalletConf struct {
+// 	Currency string
+// }
 
 // func (c Client) UserGetWallet(ctx context.Context, localVarOptionals *UserGetWalletOpts) (Wallet, *http.Response, error) {}
 
-// UserGetWalletHistoryConf
-type UserGetWalletHistoryConf struct {
-	Currency string
-	Count    float64
-	Start    float64
-}
+// type UserGetWalletHistoryConf struct {
+// 	Currency string
+// 	Count    float64
+// 	Start    float64
+// }
 
-// UserGetWalletSummaryConf
-type UserGetWalletSummaryConf struct {
-	Currency string
-}
+// type UserGetWalletSummaryConf struct {
+// 	Currency string
+// }
 
 // func (c Client) UserGetWalletSummary(ctx context.Context, localVarOptionals *UserGetWalletSummaryOpts) ([]Transaction, *http.Response, error) {}
 
-// UserMinWithdrawalFeeConf
-type UserMinWithdrawalFeeConf struct {
-	Currency string
-}
+// type UserMinWithdrawalFeeConf struct {
+// 	Currency string
+// }
 
 // func (c Client) UserMinWithdrawalFee(ctx context.Context, localVarOptionals *UserMinWithdrawalFeeOpts) (interface{}, *http.Response, error) {}
 
-// UserRequestWithdrawalConf
-type UserRequestWithdrawalConf struct {
-	OtpToken string
-	Fee      float64
-	Text     string
-}
+// type UserRequestWithdrawalConf struct {
+// 	OtpToken string
+// 	Fee      float64
+// 	Text     string
+// }
 
 // func (c Client) UserRequestWithdrawal(ctx context.Context, currency string, amount float32, address string, localVarOptionals *UserRequestWithdrawalOpts) (Transaction, *http.Response, error) {}
 
-// UserSavePreferencesConf
-type UserSavePreferencesConf struct {
-	Overwrite bool
-}
+// type UserSavePreferencesConf struct {
+// 	Overwrite bool
+// }
 
 // func (c Client) UserSavePreferences(ctx context.Context, prefs string, localVarOptionals *UserSavePreferencesOpts) (User, *http.Response, error) {}
--- rest/api_user_event.go
+++ rest/api_user_event.go
@@ -1,1 +1,8 @@
 package rest
+
+// type UserEventGetOpts struct {
+// 	Count optional.Float64
+// 	StartId optional.Float64
+// }
+
+// func (a *UserEventApiService) UserEventGet(ctx context.Context, localVarOptionals *UserEventGetOpts) ([]UserEvent, *http.Response, error) {
